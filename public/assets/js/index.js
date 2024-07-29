e2af2819d0bebcc4126c0b3016a2e7a198af3bb5
tuffythejeep <>
2024-07-28 22:13:18 -0500

    updates

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -28,3 +28,56 @@ document.addEventListener("DOMContentLoaded", () => {
       });
   }
 
+  function saveNote() {
+    const note = {
+      title: noteTitle.value,
+      text: noteText.value,
+    };
+    fetch("/api/notes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(note),
+    })
+      .then((response) => response.json())
+      .then((newNote) => {
+        fetchNotes();
+        noteTitle.value = "";
+        noteText.value = "";
+        saveNoteBtn.style.display = "none";
+        clearFormBtn.style.display = "none";
+        newNoteBtn.style.display = "inline";
+      });
+  }
+
+  function deleteNote() {
+    const selectedNote = document.querySelector(".list-group-item.selected");
+    if (selectedNote) {
+      fetch(`/api/notes/${selectedNote.dataset.id}`, {
+        method: "DELETE",
+      }).then(() => {
+        fetchNotes();
+        noteTitle.value = "";
+        noteText.value = "";
+        saveNoteBtn.style.display = "none";
+        clearFormBtn.style.display = "inline";
+      });
+    }
+  }
+
+  saveNoteBtn.addEventListener("click", saveNote);
+  clearFormBtn.addEventListener("click", () => {
+    noteTitle.value = "";
+    noteText.value = "";
+  });
+  newNoteBtn.addEventListener("click", () => {
+    noteTitle.value = "";
+    noteText.value = "";
+    saveNoteBtn.style.display = "inline";
+    clearFormBtn.style.display = "inline";
+    newNoteBtn.style.display = "none";
+  });
+
+  fetchNotes();
+});
