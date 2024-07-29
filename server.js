2305ec3cedd33c322471446950eb9e65fde02120
tuffythejeep <>
2024-07-28 21:40:10 -0500

    server piece and updates

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
