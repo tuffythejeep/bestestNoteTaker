c37f8dbf9ca79b282be3e074600a582cad7048fb
tuffythejeep <tuffythejeep@yahoo.com>
2024-07-30 00:15:17 -0500

    trouble

--- public/assets/js/notes.js
+++ public/assets/js/notes.js
@@ -5,16 +5,19 @@ document.addEventListener("DOMContentLoaded", () => {
   const saveNoteBtn = document.querySelector(".save-note");
   const clearFormBtn = document.querySelector(".clear-btn");
   const newNoteBtn = document.querySelector(".new-note");
+
   function showSaveAndClearButtons() {
     saveNoteBtn.style.display = "inline";
     clearFormBtn.style.display = "inline";
     newNoteBtn.style.display = "none";
   }
+
   function hideSaveAndClearButtons() {
     saveNoteBtn.style.display = "none";
     clearFormBtn.style.display = "none";
     newNoteBtn.style.display = "inline";
   }
+
   function handleNoteInput() {
     if (noteTitle.value.trim() !== "" || noteText.value.trim() !== "") {
       showSaveAndClearButtons();
@@ -22,8 +25,10 @@ document.addEventListener("DOMContentLoaded", () => {
       hideSaveAndClearButtons();
     }
   }
+
   noteTitle.addEventListener("input", handleNoteInput);
   noteText.addEventListener("input", handleNoteInput);
+
   function fetchNotes() {
     fetch("/api/notes")
       .then((response) => response.json())
@@ -40,6 +45,7 @@ document.addEventListener("DOMContentLoaded", () => {
       })
       .catch((error) => console.error("Error:", error));
   }
+
   function createNoteElement(note) {
     const li = document.createElement("li");
     li.className = "list-group-item";
@@ -52,11 +58,13 @@ document.addEventListener("DOMContentLoaded", () => {
     });
     return li;
   }
+
   function saveNote() {
     const note = {
       title: noteTitle.value,
       text: noteText.value,
     };
+
     fetch("/api/notes", {
       method: "POST",
       headers: {
@@ -73,11 +81,13 @@ document.addEventListener("DOMContentLoaded", () => {
       })
       .catch((error) => console.error("Error:", error));
   }
+
   function clearForm() {
     noteTitle.value = "";
     noteText.value = "";
     hideSaveAndClearButtons();
   }
+
   saveNoteBtn.addEventListener("click", saveNote);
   clearFormBtn.addEventListener("click", clearForm);
   newNoteBtn.addEventListener("click", () => {
@@ -85,6 +95,7 @@ document.addEventListener("DOMContentLoaded", () => {
     noteText.value = "";
     showSaveAndClearButtons();
   });
+
   fetchNotes();
   hideSaveAndClearButtons(); // Initially hide Save and Clear buttons
 });
